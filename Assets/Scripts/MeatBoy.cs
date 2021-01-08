e7700a725c98c295b9c8b0726599ad5caf22dedb
Patricio <jpdibacco@gmail.com>
2021-01-09 00:38:56 +0100

    latest changes with animation

--- Assets/Scripts/MeatBoy.cs
+++ Assets/Scripts/MeatBoy.cs
@@ -28,6 +28,7 @@ public class MeatBoy : MonoBehaviour{
     public Animator animator;
     public int reSpawnTime = 2;
     public GameObject bodyRotate;
+    public Rigidbody rb;
     // public GameObject respawnPosition;
     private void Awake() {
         controller = GetComponent<CharacterController>();
@@ -61,7 +62,7 @@ public class MeatBoy : MonoBehaviour{
         }
         if(controller.isGrounded){
               
-            mouvement.y = 0;
+            // mouvement.y = 0;
             jumpsCount = 0;
             // jetPackFuel++;
             // Debug.Log(jetPackFuel);
@@ -75,34 +76,23 @@ public class MeatBoy : MonoBehaviour{
         }
         // this didn't work: if(Input.GetButtonDown("Jump")){
         if(Input.GetKeyDown(KeyCode.Z)){
-            if(jumpsCount < jumpsMax){
-                jumpsCount++;
+                if(jumpsCount == 0){
                 mouvement.y = jumpSpeed;
-                //  animator.SetBool("jump", true);
-            }
-            //Debug.Log(jumpsCount);
-            // Création des gouttes quand MeatBoy Jump:
-            Instantiate(gouttePrefab, transform.position, Quaternion.identity);
-        } else if(Input.GetKeyUp(KeyCode.Z)){
-            // animator.SetBool("jump", false);
+                animator.SetBool("jump", true);
+                jumpsCount++;
+                }
+    
+        }
+         if(Input.GetKeyUp(KeyCode.Z)){
+            animator.SetBool("jump", false);
 
         }
         controller.Move(mouvement * Time.deltaTime * speed);
         // Création des gouttes quand MeatBoy court:
         if(mouvement.x != 0f){
             IsMoving = true;
-            // allAnimation();
-            // <=================animator setbool
             cptGoutte -= Time.deltaTime;
-            //Debug.Log(cptGoutte);
-            if(cptGoutte <= 0f){
-                cptGoutte = delayGoutte;
-                //Instantiate goutte... 
-                GameObject goutte = Instantiate(gouttePrefab, transform.position, Quaternion.identity) as GameObject;
-                // this is not working:
-                goutte.GetComponent<Goutte>().velocity = new Vector3(mouvement.x*speed,speed*Time.deltaTime,0f);
-                
-            }
+
         }
         if(currentHealth<=0){
             Die();
@@ -119,21 +109,26 @@ public class MeatBoy : MonoBehaviour{
         // this is only working when i destroy the character...
         //transform.position = defaultPosition;
         controller.enabled = false;
+        animator.SetBool("Death", true);
         //Destroy(gameObject);
         Invoke("MoveBody", reSpawnTime);
+        
         Debug.LogError("die");
     }
     public void MoveBody(){
         currentHealth = maxHealth;
         controller.enabled = true;
+        animator.SetBool("Death", false);
         Vector3 rp = GameObject.FindGameObjectWithTag("Respawn").transform.position;
         //Instantiate(gameObject, rp, Quaternion.identity);
         controller.Move(rp);
     }
     public void Fly(){
-       mouvement.y = 0.11f * jetPackSpeed + Time.deltaTime;
-        // mouvement.y = jetPackSpeed;
-        if(jetPackHeat > jetPackMaxHeat){
+        // Vector3 currentVector = Vector3.up;
+    //    mouvement.y = 0.11f * jetPackSpeed + Time.deltaTime;
+        // rb.AddForce(currentVector * jetPackSpeed);
+         mouvement.y = jetPackSpeed;
+        if(jetPackHeat > jetPackMaxHeat && !controller.isGrounded){
             mouvement.y = 0 - gravity/10;
         }
         if(mouvement.x == 0 ){
@@ -146,6 +141,7 @@ public class MeatBoy : MonoBehaviour{
         controller.Move(mouvement * Time.deltaTime * jetPackSpeed);
     }
     public void TakeDamage(int damage){
+        Instantiate(gouttePrefab, transform.position, Quaternion.identity);
         currentHealth -= damage;
         healthBar.setHealth(currentHealth);
     }
